e5ddade1d1203efa133c1e59995ace974d7c8ed2
Giorgos Fotopoulos <gfot@interleasedsa.onmicrosoft.com>
2023-03-08 23:39:11 +0200

    Added CartService to cater for the Shopping Cart functionality

---
+++ SmartShop/Services/CartService.cs
@@ -0,0 +1,34 @@
+﻿using SmartShop.Models;
+using System.Collections.ObjectModel;
+
+namespace SmartShop.Services;
+
+public class CartService : ICartService
+{
+	private readonly ObservableCollection<Product> _cartItems = new();
+
+	public CartService()
+	{
+		CartItems = _cartItems;
+	}
+
+	public ObservableCollection<Product> CartItems { get; }
+
+	public void AddToCart(Product product)
+	{
+		if (!CartItems.Contains(product))
+		{
+			CartItems.Add(product);
+			product.IsInCart = true;
+		}
+	}
+
+	public void RemoveFromCart(Product product)
+	{
+		if (CartItems.Contains(product))
+		{
+			CartItems.Remove(product);
+			product.IsInCart = false;
+		}
+	}
+}
---
+++ SmartShop/Services/ICartService.cs
@@ -0,0 +1,11 @@
+﻿using SmartShop.Models;
+using System.Collections.ObjectModel;
+
+namespace SmartShop.Services;
+
+public interface ICartService
+{
+	ObservableCollection<Product> CartItems { get; }
+	void AddToCart(Product product);
+	void RemoveFromCart(Product product);
+}
