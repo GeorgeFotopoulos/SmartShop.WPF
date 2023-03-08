8c4815c644a12019bafb6e26dd05ce94b9653ebc
Giorgos Fotopoulos <gfot@interleasedsa.onmicrosoft.com>
2023-03-08 23:43:55 +0200

    Removed all dependencies from ShoppingCartWindow, as they are now passed straight to the ShoppingCartViewModel

--- SmartShop/Views/ShoppingCartWindow.xaml.cs
+++ SmartShop/Views/ShoppingCartWindow.xaml.cs
@@ -1,16 +1,11 @@
-﻿using SmartShop.Models;
-using SmartShop.Services;
-using SmartShop.ViewModels;
-using System.Collections.ObjectModel;
-using System.Windows;
+﻿using System.Windows;
 
 namespace SmartShop.Views;
 
 public partial class ShoppingCartWindow : Window
 {
-	public ShoppingCartWindow(IProductService productService, ObservableCollection<Product> cartItems)
+	public ShoppingCartWindow()
 	{
 		InitializeComponent();
-		DataContext = new ShoppingCartViewModel(productService, cartItems);
 	}
 }
