3403bc729862224f2c0efa8a8a7eaaf4678400ae
Giorgos Fotopoulos <gfot@interleasedsa.onmicrosoft.com>
2023-03-09 15:03:08 +0200

    Implemented cart export functionality, in .txt file

--- SmartShop/ViewModels/ShoppingCartViewModel.cs
+++ SmartShop/ViewModels/ShoppingCartViewModel.cs
@@ -1,10 +1,15 @@
-﻿using SmartShop.Models;
+﻿using Microsoft.Win32;
+using SmartShop.Commands;
+using SmartShop.Models;
 using SmartShop.Services;
 using SmartShop.Utilities;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
+using System.IO;
 using System.Linq;
+using System.Text;
+using System.Windows.Input;
 
 namespace SmartShop.ViewModels;
 
@@ -41,11 +46,15 @@ public class ShoppingCartViewModel : PropertyChangedBase
 		{
 			product.PropertyChanged += OnProductPropertyChanged;
 		}
+
+		ExportCartCommand = new RelayCommand(ExportCart, () => SklavenitisProducts.Concat(AbProducts).Any(product => product.IsInCart));
 	}
 
 	public ObservableCollection<Product> SklavenitisProducts { get => _sklavenitisProducts; set => SetField(ref _sklavenitisProducts, value); }
 	public ObservableCollection<Product> AbProducts { get => _abProducts; set => SetField(ref _abProducts, value); }
 	public double TotalPrice => SklavenitisProducts.Concat(AbProducts).Where(product => product.IsInCart).Sum(product => product.FinalPrice);
+
+	public ICommand ExportCartCommand { get; }
 
 	private void OnProductPropertyChanged(object sender, PropertyChangedEventArgs e)
 	{
@@ -53,6 +62,40 @@ public class ShoppingCartViewModel : PropertyChangedBase
 		if (e.PropertyName == nameof(Product.IsInCart))
 		{
 			NotifyPropertyChanged(nameof(TotalPrice));
+		}
+	}
+
+	public void ExportCart()
+	{
+		var productsInCart = SklavenitisProducts.Concat(AbProducts).Where(product => product.IsInCart);
+		var fileContent = new StringBuilder();
+
+		foreach (var product in productsInCart)
+		{
+			fileContent.AppendLine($"Κατάστημα: {product.Store}");
+			fileContent.AppendLine($"Προϊόν: {product.ProductName}");
+			fileContent.AppendLine($"Σύνδεσμος: {product.Link}");
+			fileContent.AppendLine($"Τιμή: {product.FinalPrice:F2} €");
+			fileContent.AppendLine($"Τιμή ανά μονάδα: {product.PricePerUnitWithMetricUnit}");
+			if (product.DiscountPercentage.HasValue)
+			{
+				fileContent.AppendLine($"Ποσοστό έκπτωσης: {product.DiscountPercentage.Value.ToString("P2")}");
+			}
+
+			fileContent.AppendLine();
+		}
+
+		fileContent.AppendLine($"\nΣύνολο: {TotalPrice:F2} €");
+
+		var saveFileDialog = new SaveFileDialog
+		{
+			Filter = "Text Files (*.txt)|*.txt",
+			FileName = "ShoppingCart.txt"
+		};
+
+		if (saveFileDialog.ShowDialog() == true)
+		{
+			File.WriteAllText(saveFileDialog.FileName, fileContent.ToString());
 		}
 	}
 }
